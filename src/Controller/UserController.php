e20fea0b250ec42f2faab80afbeeec1380879668
Anatis2 <claire.coubard@gmail.com>
2020-12-14 12:00:51 +0100

    UserController : mise en place de la méthode createAction()

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,26 +2,54 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserController extends AbstractController
 {
 	/**
 	 * @Route("/users", name="user_list")
 	 */
-	public function listAction()
+	public function listAction(UserRepository $userRepository)
 	{
-        //TODO
+		$users = $userRepository->findAll();
+
+		return $this->render('user/list.html.twig', [
+			'users' => $users,
+		]);
     }
 
 	/**
 	 * @Route("/users/create", name="user_create")
 	 */
-	public function createAction()
+	public function createAction(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $encoder)
 	{
-		//TODO
+		$user = new User();
+		$form = $this->createForm(UserType::class, $user);
+
+		$form->handleRequest($request);
+
+		if($form->isSubmitted() && $form->isValid()) {
+			$password = $encoder->encodePassword($user, $user->getPassword());
+			$user->setPassword($password);
+			$user->setRoles(['ROLE_USER']);
+			$em->persist($user);
+			$em->flush();
+
+			$this->addFlash('success', "L'utilisateur a bien été ajouté.");
+			return $this->redirectToRoute('user_list');
+		}
+
+		return $this->render('user/create.html.twig', [
+			'form' => $form->createView()
+		]);
 	}
 
 	/**
