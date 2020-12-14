9a7ff3544688aef064f486ed356a35354e616cce
Anatis2 <claire.coubard@gmail.com>
2020-12-14 17:54:55 +0100

    TaskController : mise en place de la méthode create()

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
+use App\Form\TaskType;
+use App\Repository\TaskRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,17 +16,37 @@ class TaskController extends AbstractController
     /**
      * @Route("/tasks", name="task_list")
      */
-    public function listAction()
+    public function listAction(TaskRepository $taskRepository)
     {
-        //TODO
+        $tasks = $taskRepository->findAll();
+
+        return $this->render('task/list.html.twig', [
+        	'tasks' => $tasks,
+		]);
     }
 
 	/**
 	 * @Route("/tasks/create", name="task_create")
 	 */
-	public function createAction(Request $request)
+	public function createAction(Request $request, EntityManagerInterface $em)
 	{
+		$task = new Task();
+		$form = $this->createForm(TaskType::class, $task);
 
+		$form->handleRequest($request);
+
+		if($form->isSubmitted() && $form->isValid()) {
+			$em->persist($task);
+			$em->flush();
+
+			$this->addFlash('success', 'La tâche a été bien été ajoutée.');
+
+			return $this->redirectToRoute('task_list');
+		}
+
+		return $this->render('task/create.html.twig', [
+			'form' => $form->createView()
+		]);
 	}
 
 	/**
