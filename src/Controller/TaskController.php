275d09808471f7d67b9645284f43afff8a2a9d8c
Anatis2 <claire.coubard@gmail.com>
2020-12-23 10:44:54 +0100

    modifié message addFlash quand tâche marquée comme non faite

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -83,7 +83,12 @@ class TaskController extends AbstractController
 		$task->toggle(!$task->isDone());
 		$em->flush();
 
-		$this->addFlash('success', sprintf('La tâche %s a bien été marquée comme faite.', $task->getTitle()));
+		if($task->isDone()) {
+			$this->addFlash('success', sprintf('La tâche %s a bien été marquée comme faite.', $task->getTitle()));
+		} else {
+			$this->addFlash('success', sprintf('La tâche %s a bien été rétrogradée comme non faite.', $task->getTitle()));
+		}
+
 		return $this->redirectToRoute('task_list');
 	}
 
