66cc61a8c8d09bcf4497e99b839abe23724b779f
Anatis2 <claire.coubard@gmail.com>
2021-01-13 14:11:34 +0100

    suppression d'un dump()

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -13,8 +13,6 @@ class DefaultController extends AbstractController
      */
     public function home(): Response
     {
-    	$salt = $this->getUser();
-    	dump($salt);
         return $this->render('homePage.html.twig');
     }
 }
